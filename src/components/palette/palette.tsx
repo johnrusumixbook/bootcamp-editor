0ac1ac00ac5c182a9e8903c5bbd06d8e9a8d8f95
JohnRusu <johnrusumixbook@gmail.com>
2022-05-25 09:49:50 +0300

    props naming issue

--- src/components/palette/palette.tsx
+++ src/components/palette/palette.tsx
@@ -3,11 +3,11 @@ import ShapeModel from "../../data/shapeModel";
 import "./palette.css";
 import Shape from "./shape/shape";
 
-interface IPaletteModel {
+interface IPaletteProps {
   items: ShapeModel[];
 }
 
-const Palette = (props: IPaletteModel) => {
+const Palette = (props: IPaletteProps) => {
   return (
     <div className="palette">
       {props.items.map((item, i) => (
